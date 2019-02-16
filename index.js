049796cd123693a73617bd7571ae5f719ffc5d3e
Michael Maggs <michaelmaggs@gmail.com>
2019-02-16 20:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-// Add your functions and code here
 function destructivelyAppendKitten(name) {
   kittens.push(name)
+  return kittens
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
-}
-
-function destructivelyRemoveLastKitten() {
-  kittens.pop()
-}
-
-function destructivelyRemoveFirstKitten() {
-  kittens.shift()
-}
-
-function appendKitten(name) {
-  var kitten = [...kittens, name]
-  return kitten
-}
-
-function prependKitten(name) {
-  var kitten = [name, ...kittens]
-  return kitten
-}
-
-function removeLastKitten() {
-  return kittens.slice(0,-1)
-}
-
-function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens
 }
